537237d42b85ae4928dfa6d003bb8816cad55e67
Karim Wahdan <dr.karimwahdan@gmail.com>
2024-04-17 17:33:23 +0200

    init

--- rsv/src/main/java/com/karimwahdan/rsv/cards/NotificationCard.kt
+++ rsv/src/main/java/com/karimwahdan/rsv/cards/NotificationCard.kt
@@ -31,39 +31,45 @@ fun NotificationCard(
     notification:ProjectNotification,
     logo:Int,
 ){
+    val title=notification.title
+    val message=notification.message
+    val createdAt=notification.created_at
     Box(modifier = Modifier.padding(10.dp)){
         Column(modifier= Modifier.background(backgroundColor)
             .border(width = 1.dp,color = borderColor,shape = RoundedCornerShape(10.dp))
             .fillMaxWidth().padding(10.dp)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
                     modifier= Modifier.size(52.dp),
                     painter = painterResource(logo),
                     contentDescription =null )
                 Column(modifier= Modifier.fillMaxWidth().weight(1f),
                     verticalArrangement = Arrangement.SpaceBetween){
-                    Text(
-                        modifier=Modifier.fillMaxWidth(),
-                        fontWeight = FontWeight.Bold,
-                        maxLines=1,
-                        overflow = TextOverflow.Ellipsis,
-                        text = notification.title
-                    )
-                    Text(
-                        modifier=Modifier.fillMaxWidth(),
-                        maxLines=1,
-                        overflow = TextOverflow.Ellipsis,
-                        text = notification.message,
-                    )
+                    if(title!=null){
+                         Text(
+                            modifier=Modifier.fillMaxWidth(),
+                            fontWeight = FontWeight.Bold,
+                            maxLines=1,
+                            overflow = TextOverflow.Ellipsis,
+                            text = title
+                        )
+                    }
+                    if (message!=null){
+                        Text(
+                            modifier=Modifier.fillMaxWidth(),maxLines=1,
+                            overflow = TextOverflow.Ellipsis,text = message,
+                        )
+                    }
                 }
             }
-            Text(modifier=Modifier.fillMaxWidth(),
-                textAlign = TextAlign.End,
-                fontSize = 12.sp,
-                text = notification.created_at?:"")
+            if (createdAt!=null){
+                Text(modifier=Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End,
+                    fontSize = 12.sp,
+                    text = notification.created_at?:"")
+            }
+
         }
     }
 
